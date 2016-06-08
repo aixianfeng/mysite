859de7b9a239dfe0a125251d36eba06b2df03502
aixianfeng <xianfengai@gmail.com>
2016-06-08 16:04:33 +0800

    changed urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,4 +19,6 @@ urlpatterns = patterns('',
 	url(r'^hello/$',hello),
 	url(r'^time/$', current_datetime),
 	url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+	
+	url(r'^$', 'mysite.views.home', name='home'),
 )
