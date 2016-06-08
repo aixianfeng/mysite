7d793c7311a70f2298f1086b69ea085e15a4f212
aixianfeng <xianfengai@gmail.com>
2016-06-08 22:30:38 +0800

    add mydata.db

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,31 @@
+# -*- coding: utf-8 -*-
+
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import Http404
+
+from django.contrib.syndication.views import Feed
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from books.models import Article
+from books.models import Tag
 import datetime
+from datetime import datetime
+
+# Create your views here
+def home(request):
+	posts = Article.objects.all()
+	paginator = Paginator(posts, 2)
+	page = request.GET.get('page')
+
+	try:
+		post_list = paginator.page(page)
+	except PageNotAnInteger:
+		post_list = paginator.page(1)
+	except EmptyPage:
+		post_list = paginator.paginator(paginator.num_pages)
+	return render(request, 'home.html', {'post_list': post_list})
 
 def hello(request):
 	return HttpResponse("Hello World!")
